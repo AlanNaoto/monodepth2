1bf45cc13f54d31071b5be79208ececeabb9cc00
alan_inhealth <naoto321@gmail.com>
2020-03-16 16:18:22 +0100

    added new evaluation script which doesn't loads all predictions into memory at once...

--- evaluate_depth_all_models_less_memory.py
+++ evaluate_depth_all_models_less_memory.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
+import tempfile
 import os
 import cv2
 import numpy as np
@@ -56,17 +57,7 @@ def batch_post_process_disparity(l_disp, r_disp):
     return r_mask * l_disp + l_mask * r_disp + (1.0 - l_mask - r_mask) * m_disp
 
 
-def evaluate(opt, split, out_dir):
-    """Evaluates a pretrained model using a specified test set
-    """
-    #MIN_DEPTH = 1e-3
-    #MAX_DEPTH = 80
-    MIN_DEPTH = opt.min_depth
-    MAX_DEPTH = opt.max_depth
-
-    assert sum((opt.eval_mono, opt.eval_stereo)) == 1, \
-        "Please choose mono or stereo evaluation by setting either --eval_mono or --eval_stereo"
-
+def get_pred_disps(opt, split, tmp_dir_path, out_dir):
     if opt.ext_disp_to_eval is None:
 
         opt.load_weights_folder = os.path.expanduser(opt.load_weights_folder)
@@ -77,7 +68,7 @@ def evaluate(opt, split, out_dir):
         print("-> Loading weights from {}".format(opt.load_weights_folder))
         #filenames = readlines(os.path.join(splits_dir, opt.eval_split, "test_files.txt"))
         train_or_val = {"train": "train_files.txt", "val": "val_files.txt"}    
-        filenames = readlines(os.path.join(splits_dir, opt.eval_split, train_or_val[split]))
+        filenames = sorted(readlines(os.path.join(splits_dir, opt.eval_split, train_or_val[split])))  # sorted facilitates our life
         encoder_path = os.path.join(opt.load_weights_folder, "encoder.pth")
         decoder_path = os.path.join(opt.load_weights_folder, "depth.pth")
 
@@ -86,8 +77,8 @@ def evaluate(opt, split, out_dir):
         dataset = datasets.carla_dataset.CarlaDataset(opt.data_path, filenames,
                                            encoder_dict['height'], encoder_dict['width'],
                                            [0], 4, is_train=False)
-        dataloader = DataLoader(dataset, 16, shuffle=False, num_workers=opt.num_workers,
-                                pin_memory=True, drop_last=False)
+        dataloader = DataLoader(dataset, 1, shuffle=False, num_workers=opt.num_workers,
+                                pin_memory=True, drop_last=False)  # Changed batch from 16 to 1 (before was evaluating only total/16 it seems?)
 
         encoder = networks.ResnetEncoder(opt.num_layers, False)
         depth_decoder = networks.DepthDecoder(encoder.num_ch_enc)
@@ -108,8 +99,8 @@ def evaluate(opt, split, out_dir):
 
         with torch.no_grad():
             for frame_idx, data in enumerate(dataloader):
-                if frame_idx % 50 == 0:
-                    print(f"Processing {frame_idx}/{len(dataloader)}")
+                if frame_idx % 100 == 0:
+                    print(f"Creating disparity {frame_idx}/{len(dataloader)}")
 
                 input_color = data[("color", 0, 0)].cuda()
 
@@ -126,90 +117,66 @@ def evaluate(opt, split, out_dir):
                     N = pred_disp.shape[0] // 2
                     pred_disp = batch_post_process_disparity(pred_disp[:N], pred_disp[N:, :, ::-1])
 
-                pred_disps.append(pred_disp)
+                #pred_disps.append(pred_disp)
+                frame_name = filenames[frame_idx]
+                output_path = os.path.join(tmp_dir_path, f"{frame_name}.npy")
+                np.save(output_path, pred_disp)
+#        pred_disps = np.concatenate(pred_disps)
+    
+    #output_path = os.path.join(opt.load_weights_folder, "disps_{}_split.npy".format(opt.eval_split))
+#    print("-> Saving predicted disparities to ", output_path)
+    print(f"Saved predicted disparities to temporary dir {tmp_dir_path}")
+    disparity_files = [os.path.join(tmp_dir_path, x) for x in os.listdir(tmp_dir_path)]
+    disparity_files = sorted(disparity_files)
+    return disparity_files, filenames
 
-        pred_disps = np.concatenate(pred_disps)
 
-    else:
-        # Load predictions from file
-        print("-> Loading predictions from {}".format(opt.ext_disp_to_eval))
-        pred_disps = np.load(opt.ext_disp_to_eval)
+def evaluate(opt, split, dataset, tmp_dir_root, out_dir):
+    """Evaluates a pretrained model using a specified test set
+    """
+    #MIN_DEPTH = 1e-3
+    #MAX_DEPTH = 80
+    MIN_DEPTH = opt.min_depth
+    MAX_DEPTH = opt.max_depth
 
-        if opt.eval_eigen_to_benchmark:
-            eigen_to_benchmark_ids = np.load(
-                os.path.join(splits_dir, "benchmark", "eigen_to_benchmark_ids.npy"))
+    assert sum((opt.eval_mono, opt.eval_stereo)) == 1, \
+        "Please choose mono or stereo evaluation by setting either --eval_mono or --eval_stereo"
 
-            pred_disps = pred_disps[eigen_to_benchmark_ids]
-
-    if opt.save_pred_disps:
-        output_path = os.path.join(
-            opt.load_weights_folder, "disps_{}_split.npy".format(opt.eval_split))
-        print("-> Saving predicted disparities to ", output_path)
-        np.save(output_path, pred_disps)
-
-    if opt.no_eval:
-        print("-> Evaluation disabled. Done.")
-        quit()
-
-    elif opt.eval_split == 'benchmark':
-        save_dir = os.path.join(opt.load_weights_folder, "benchmark_predictions")
-        print("-> Saving out benchmark predictions to {}".format(save_dir))
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-
-        for idx in range(len(pred_disps)):
-            disp_resized = cv2.resize(pred_disps[idx], (1216, 352))
-            depth = STEREO_SCALE_FACTOR / disp_resized
-            depth = np.clip(depth, 0, 80)
-            depth = np.uint16(depth * 256)
-            save_path = os.path.join(save_dir, "{:010d}.png".format(idx))
-            cv2.imwrite(save_path, depth)
-
-        print("-> No ground truth is available for the KITTI benchmark, so not evaluating. Done.")
-        quit()
+    tmp = tempfile.TemporaryDirectory(dir=tmp_dir_root)
+    tmp_dir_path = tmp.name
+    disparity_files, split_filenames = get_pred_disps(opt, split, tmp_dir_path, out_dir)
 
     #gt_path = os.path.join(splits_dir, opt.eval_split, "gt_depths.npz")
     # gt_depths = np.load(gt_path, fix_imports=True, encoding='latin1')["data"]
     gt_dir = os.path.join(opt.data_path, "depth_npy")
-    gt_depths = [os.path.join(gt_dir, x) for x in os.listdir(gt_dir)]
+    gt_depths = {x: os.path.join(gt_dir, x) for x in os.listdir(gt_dir)}
 
     print("-> Evaluating")
-
-    if opt.eval_stereo:
-        print("   Stereo evaluation - "
-              "disabling median scaling, scaling by {}".format(STEREO_SCALE_FACTOR))
-        opt.disable_median_scaling = True
-        opt.pred_depth_scale_factor = STEREO_SCALE_FACTOR
-    else:
-        print("   Mono evaluation - using median scaling")
+    print("   Mono evaluation - using median scaling")
 
     errors = []
     ratios = []
-
-    for i in range(pred_disps.shape[0]):
+    for i in range(len(split_filenames)):
         if i % 100 == 0:
-            print(f"{i}/{pred_disps.shape[0]}")
+            print(f"Computing error {i}/{len(split_filenames)}")
 
-        # gt_depth = gt_depths[i]
-        gt_depth = np.load(gt_depths[i])
+        gt_filename = split_filenames[i] + ".npy"  # This is done since predict is a subset from gt
+        if dataset == 'waymo':
+            gt_depth = np.zeros((1280, 1920))  # Original resolution (height, width)
+            lidar_data = np.load(gt_depths[gt_filename])
+            for lidar_point in lidar_data:
+                gt_depth[int(lidar_point[1])][int(lidar_point[0])] = lidar_point[2]
+            gt_depth = cv2.resize(gt_depth, (1024, 320))
+        else:
+            gt_depth = np.load(gt_depths[gt_filename])
+
         gt_height, gt_width = gt_depth.shape[:2]
-
-        pred_disp = pred_disps[i]
-        pred_disp = cv2.resize(pred_disp, (gt_width, gt_height))
+        pred_disp = np.load(disparity_files[i])
+        pred_disp = pred_disp[0]
+        #pred_disp = cv2.resize(pred_disp, (gt_width, gt_height))  # FIXME My GT is already the same shape as the predictions
         pred_depth = 1 / pred_disp
 
-        if opt.eval_split == "eigen":
-            mask = np.logical_and(gt_depth > MIN_DEPTH, gt_depth < MAX_DEPTH)
-
-            crop = np.array([0.40810811 * gt_height, 0.99189189 * gt_height,
-                             0.03594771 * gt_width,  0.96405229 * gt_width]).astype(np.int32)
-            crop_mask = np.zeros(mask.shape)
-            crop_mask[crop[0]:crop[1], crop[2]:crop[3]] = 1
-            mask = np.logical_and(mask, crop_mask)
-
-        else:
-            mask = gt_depth > 0
-
+        mask = gt_depth > 0
         pred_depth = pred_depth[mask]
         gt_depth = gt_depth[mask]
 
@@ -243,6 +210,7 @@ def evaluate(opt, split, out_dir):
         f.write(f"Scaling ratios\n")
         std_scale = np.std(ratios/med)
         f.write(f"med: {med:.5f}  std: {std_scale:.5f}")
+    tmp.cleanup()
 
 
 if __name__ == "__main__":
@@ -250,12 +218,14 @@ if __name__ == "__main__":
     out_dir = "/home/alan/workspace/mestrado/monodepth2_results/waymo_1024x320/metrics"
     options = MonodepthOptions()
     opts = options.parse()
-    opts.eval_mono = True
+    opts.eval_mono = True    
     opts.eval_split = "waymo_1024x320"  # Testing on complete carla 
     opts.data_path = "/home/alan/workspace/mestrado/dataset/WAYMO_1024x320"
     #opts.load_weights_folder = "/media/aissrtx2060/Naotop_1TB1/monodepth2_data/carla_1024x320_full/carla_1024x320/models/weights_9"
     opts.max_depth = 75.0
     opts.min_depth = 0.1
+    dataset = 'waymo'
+    tmp_dir_root = "/home/alan/workspace/mestrado/temp"
     splits = ['train', 'val']
 
     model_paths = [os.path.join(dir_model_weights, model) for model in os.listdir(dir_model_weights) if os.path.isdir(os.path.join(dir_model_weights, model))]
@@ -263,4 +233,4 @@ if __name__ == "__main__":
         opts.load_weights_folder = model_path
         for split in splits:
             os.makedirs(os.path.join(out_dir, split), exist_ok=True)
-            evaluate(opts, split, out_dir)
+            evaluate(opts, split, dataset, tmp_dir_root, out_dir)
